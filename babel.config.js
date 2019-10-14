be0ddb627bf284a1ddc34324ef5c779f346d8bb9
calanchenlins <calanchenlins@Outlook.com>
2019-10-14 21:02:55 +0800

    upload babel

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   presets: [
     '@vue/app'
+  ],
+  plugins: [// 按需引入 element-ui
+    [
+      'component',
+      {
+        'libraryName': 'element-ui',
+        'styleLibraryName': 'theme-chalk'
+      }
+    ]
   ]
 }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+import { Container,
+  Header,
+  Aside,
+  Main,
+  Footer, input, Switch, tree, divider, tooltip, button, Collapse, CollapseItem, card, table, TableColumn, Col, Row } from 'element-ui'
 
-Vue.use(ElementUI)
+// Vue.use(ElementUI)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Footer)
+Vue.use(input)
+Vue.use(Switch)
+Vue.use(tree)
+Vue.use(divider)
+Vue.use(tooltip)
+Vue.use(button)
+Vue.use(Collapse)
+Vue.use(CollapseItem)
+Vue.use(card)
+Vue.use(table)
+Vue.use(Row)
+Vue.use(TableColumn)
+Vue.use(Col)
 Vue.config.productionTip = false
 
 new Vue({
